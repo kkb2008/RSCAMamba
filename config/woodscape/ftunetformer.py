8bec63849c4ecf8ee1f24b46003912b8e2c4d6e4
ZX_zzz <147788011+kkb2008@users.noreply.github.com>
2025-11-17 21:22:28 +0800

    Update ftunetformer.py

--- config/woodscape/ftunetformer.py
+++ config/woodscape/ftunetformer.py
@@ -6,7 +6,7 @@ from tools.utils import Lookahead
 from tools.utils import process_model_params
 
 # training hparam
-max_epoch = 45
+max_epoch = 60
 ignore_index = len(CLASSES)
 train_batch_size = 8
 val_batch_size = 4
@@ -68,6 +68,7 @@ net_params = process_model_params(net, layerwise_params=layerwise_params)
 base_optimizer = torch.optim.AdamW(net_params, lr=lr, weight_decay=weight_decay)
 optimizer = Lookahead(base_optimizer)
 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=15, T_mult=2)
+
 
 
 
